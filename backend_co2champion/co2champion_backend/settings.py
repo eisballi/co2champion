67f511edb7a68e0556b4d9e401ebdda3b2c52fb4
Sorush Shahidy <ahmed.shahidy@edu.fh-joanneum.at>
2025-01-25 13:57:18 +0100

    settings.py fix

--- backend_co2champion/co2champion_backend/settings.py
+++ backend_co2champion/co2champion_backend/settings.py
@@ -129,11 +129,14 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-'DEFAULT_AUTHENTICATION_CLASSES': (
-'rest_framework_simplejwt.authentication.JWTAuthentication',
-'rest_framework.authentication.BasicAuthentication',
-)
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
 }
+
 
 LOGGING = {
     'version': 1,
